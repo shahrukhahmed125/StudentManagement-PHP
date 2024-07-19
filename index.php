9be5571f88765530c444885bc357b5c18100a5b8
shahrukhahmed125 <shahrukhahmed125@gmail.com>
2024-07-19 21:17:52 +0500

    second commit

--- index.php
+++ index.php
@@ -4,12 +4,34 @@
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <title>Student Management System</title>
-        <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <h1>
-        this is the management system for students.
-    </h1>
+    <nav class="navbar navbar-expand-lg bg-info" style="padding-top:15px;padding-bottom:15px;">
+        <div class="container-fluid">
+            <a class="navbar-brand fs-4 fw-bold" href="#">TechNik School</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 pl-3 pr-3">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+                </li>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Admission</a>
+                </li>
+            </ul>
+            <div class="d-flex">
+                <button class="btn btn-success" type="submit">Login</button>
+            </div>
+            </div>
+        </div>
+    </nav>
 </body>
-<script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js'></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </html>
